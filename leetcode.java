5e6186a53d6fe68b7c6b68fc0dfe7e434eb0439f
0315kumarchetan <0315kumarchetan@gmail.com>
2021-01-10 01:24:39 +0530

    leetcode 39,40

--- leetcode.java
+++ leetcode.java
@@ -29,5 +29,40 @@ public class leetcode {
     //=======================END=================================================
 
 
+    //question 40============start===============================================
+    class Solution {
+        List<Integer> smallAns = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        public void combination(int[] arr , int idx , int target){
+            if(idx==arr.length || target == 0){
+                if(target==0){
+                    res.add(new ArrayList<>(smallAns));
+                }
+                return ;
+            }
+            int prev =-1;
+            for(int i =idx ; i< arr.length;i++){
+                if(prev!=-1 && arr[prev]==arr[i]){
+                    continue;
+                }
+                if(target-arr[i]>=0){
+                    smallAns.add(arr[i]);
+                    combination(arr,i+1,target-arr[i]);
+                    smallAns.remove(smallAns.size()-1);
+                }
+                 prev=i;
+            }         
+            
+            
+        }
+        public List<List<Integer>> combinationSum2(int[] arr, int target) {
+            Arrays.sort(arr);
+            combination(arr,0,target);
+            return res;
+            
+        }
+    }
+    //===========================end===========================================
+
 
 }
